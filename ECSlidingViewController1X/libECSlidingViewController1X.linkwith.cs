b3a3613404262ea651f9d9cd705b84bfeae2bbaf
Israel Soto <israel.spgh@gmail.com>
2014-06-16 20:20:00 -0500

    [Compatibility]Arm 64 and Simulator 64 Architectures added
    «This is released under MIT lincese»

--- ECSlidingViewController1X/libECSlidingViewController1X.linkwith.cs
+++ ECSlidingViewController1X/libECSlidingViewController1X.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libECSlidingViewController1X.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, Frameworks = "UIKit CoreGraphics QuartzCore", ForceLoad = true)]
+[assembly: LinkWith ("libECSlidingViewController1X.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, Frameworks = "UIKit CoreGraphics QuartzCore", ForceLoad = true)]
